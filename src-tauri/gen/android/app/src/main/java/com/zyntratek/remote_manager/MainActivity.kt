d56b501f3c29ff67eae996654a3b309f970c6869
Danvb15 <danieleliasvasquezbarrios31@gmail.com>
2026-08-18 21:54:32 -0500

    feat: add android support and github actions apk builder

---
+++ src-tauri/gen/android/app/src/main/java/com/zyntratek/remote_manager/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zyntratek.remote_manager
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
